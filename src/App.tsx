57215315fec6c7309838f75dc5370899ab7471ff
Misha Zoubarev <mishazoubarevv@gmail.com>
2025-01-14 21:09:41 -0800

    Reload Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import { motion } from "framer-motion";
+import { faker } from "@faker-js/faker"
+import RestartButton from "./components/RestartButton";
 
-function App() {
-  return (
-    <div className="text-4xl text-center text-blue-500">
-      It works
-    </div>
+const words = Array.from({ length: 10 }, () => faker.word.sample());
+
+
+const App = () => {
+  return(
+    <>
+      <CountdownTimer timeLeft={30} />
+      <GeneratedWords words={words.join(" ")} />;
+      <RestartButton
+        className={"mx-auto mt-10 text-slate-500"} 
+        onRestart={() => null}
+      />
+    </>
   );
-}
+};
+
+const GeneratedWords = ({words}: {words: string}) => {
+    return <div className="text-4xl text-center text-slate-500">{words}</div>
+};
+
+const CountdownTimer = ({timeLeft}: {timeLeft: number}) => {
+  return <h2 className = "text-primary-400 font-medium">Time: {timeLeft}</h2>
+};
 
 export default App;
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.yellow,
+      },
+    },
   },
   plugins: [],
 }
